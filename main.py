958c21b708134274f03e7437590a0b91b90b564d
Berke <berke0943@icloud.com>
2025-03-06 11:32:36 +0000

    Add Person class

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 class Person:
-    def __init__(self, name, age):
+    def __init__(self, name, age,address):
         self.name = name
         self.age = age
+        self.address = address
 
-p1 = Person("Berke", 20)
+p1 = Person("Berke", 20,"obrzezna 2")
 
 print(p1.name)
 print(p1.age)
+print(p1.address)
