9effa76395c8208f78da015b3ec980e048f8cf6c
Berke <berke0943@icloud.com>
2025-03-06 12:16:56 +0000

    Add Person class

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
-class Person:
-    def __init__(self, name, age,address):
-        self.name = name
-        self.age = age
-        self.address = address
+class Student:
+    def __init__(self, first_name, last_name, index_number, nationality):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.index_number = index_number
+        self.nationality = nationality
 
-p1 = Person("Berke", 20,"obrzezna 2")
+    def __str__(self):
+        return f"Student(name: {self.first_name}, Last name: {self.last_name}, index_number: {self.index_number}, nationality: {self.nationality})"
 
-print(p1.name)
-print(p1.age)
-print(p1.address)
+s1 = Student("Berke", "Haliloglu", 35274, "Turkish")
+s2 = Student("Serra", "Ozbek", 35248, "Turkish")
+s3 = Student("Sirri", "Cataloglu", 34854, "Turkish")
+
+print(s1)
+print(s2)
+print(s3)
