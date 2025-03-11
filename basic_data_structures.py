1c3d4515cd0be725c65010e43eabeee9ca110deb
Berke <berke0943@icloud.com>
2025-03-11 10:35:15 +0000

    Assign data structures to variables

---
+++ basic_data_structures.py
@@ -0,0 +1,10 @@
+my_age = 20 # integer
+float_number = 3.141
+name = "Berke Haliloglu"  #strings
+list_color =["blue" , "pink" ," red"] #list
+student_info = {"name":"Berke","age":20} #dictionaries
+tuple_exmpl =(10,20,30,"forty") #tuples
+color ={"blue" , "pink", "red"} #sets
+rainy_weather = False #booleans
+
+print(my_age, float_number ,name, list_color,student_info,tuple_exmpl,color,rainy_weather )
