be907c2e6f690afd08fbbf920c095570a3364bef
Berke <berke0943@icloud.com>
2025-03-13 11:47:19 +0000

    Add Person class

--- student_details.py
+++ student_details.py
@@ -1,14 +1,10 @@
-first_name= "Berke"
-last_name= "Haliloglu "
-index_number= 35274
-nationality="Turkish"
-starting_date ="03-09-2024"
-courses= ["mathematice","English","computer science"]git add .
+student1= {
+    'first_name':'Berke',
+    'last_name':'Haliloglu',
+    'index_number':'35274',
+    'nationality':'Turkish',
+    'starting_date':'03-09-2024',
+    'courses':["mathematice","English","computer science"]
+    }
 
-
-print("First Name:", first_name)
-print("Last Name:", last_name)
-print("Index Number:", index_number)
-print("Nationality:", nationality)
-print("Starting Date:", starting_date)
-print("Courses:", courses)
+print(student1)
