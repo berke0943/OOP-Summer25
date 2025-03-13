b5d674680d2cfaf5bb5ebcc944020dea9dfc820e
Berke <berke0943@icloud.com>
2025-03-13 12:01:00 +0000

    Add Person class

--- student_details.py
+++ student_details.py
@@ -4,7 +4,26 @@ student1= {
     'index_number':'35274',
     'nationality':'Turkish',
     'starting_date':'03-09-2024',
-    'courses':["mathematice","English","computer science"]
+    'courses':["mathematics","English","computer science"]
     }
+student2 ={
+'first_name' : "Serra",
+'last_name' : "Ozbek",
+'index_number' : 35248,
+'nationality' : "Turkey",
+'starting_date' : '03.11.2024',
+'courses' : ["math","computer science", "english"]
+}
+student3 ={
+'first_name' : "Sirri",
+'last_name' : "Cataloglu",
+'index_number' : 34854,
+'nationality' : "Turkey",
+'starting_date' : '03.09.2024',
+'courses' : ["math","computer science", "english"]
+}
+
 
 print(student1)
+print(student2)
+print(student3)
