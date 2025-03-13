6a1f37743af3471a44cd900aebd9b865cc4d76fe
Berke <berke0943@icloud.com>
2025-03-13 11:02:01 +0000

    Add Person Student Class

---
+++ student_details.py
@@ -0,0 +1,10 @@
+student_info = {"Berke", "Haliloglu", 35274, "Turkish", "03-09-2023"}
+
+first_name, last_name, index_number, nationality, starting_date = list(student_info)
+
+
+print("First Name:", first_name)
+print("Last Name:", last_name)
+print("Index Number:", index_number)
+print("Nationality:", nationality)
+print("Starting Date:", starting_date)
