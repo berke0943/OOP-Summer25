fabc93ad35113a7364ed7fd11470d377954e74cb
Berke <berke0943@icloud.com>
2025-04-08 11:32:29 +0000

    My Assignmments

---
+++ my_assignments.py
@@ -0,0 +1,65 @@
+#1. Question 
+print("Berke")
+
+#2. Question 
+my_age=20
+print(my_age)
+
+#3. Question
+name="Berke"
+my_age=20
+print(type(name))
+print(type(my_age))
+
+#4. Question
+x=5
+if x > 5 :
+    print("X is greater than 5")
+elif x==5:
+    print("X equal to 5")
+else:
+    print("X is less than 5")
+
+#5. Question
+fruits = ["apple", "pear", "banana", "strawberry", "orange"]
+print(fruits)
+
+#6. Question 
+fruits = ["apple", "pear", "banana", "strawberry", "orange"]
+print(fruits)
+for fruit in fruits:
+    print(fruit)
+
+
+
+#7. Question
+student= {
+  "name": "Berke",
+  "index_numver": 35274,
+  "faculty": "engineering"
+}
+print(student)
+
+#8. Question
+class Car:
+    def __init__(self,name,model):
+        self.name=name
+        self.model=model
+
+car1 = Car("Toyota", "Corolla")
+car2 = Car("Ford", "Mustang")
+
+print(car1.model)
+print(car2.name)
+
+#9. Question 
+for i in range(1, 11):
+    print(i)
+
+#10. Question
+fruits = ["apple", "banana", "orange"]
+print(fruits[-1]) 
+
+fruits.append("kiwi")  
+print(fruits)  
+
