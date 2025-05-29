bad36804cd504d280c721ea5ba9d8dfc32a1806c
Berke <berke0943@icloud.com>
2025-05-29 10:32:20 +0000

    My QR code

---
+++ qr_code_generator.py
@@ -0,0 +1,20 @@
+import qrcode
+
+data = "https://preview--kolay-web-siteci-merhaba.lovable.app/"
+
+
+qr = qrcode.QRCode(
+    version=1, 
+    error_correction=qrcode.constants.ERROR_CORRECT_L,  
+    box_size=10,  
+    border=4,  
+)
+
+qr.add_data(data) 
+qr.make(fit=True)  
+
+img = qr.make_image(fill_color="black", back_color="white") 
+
+img.save("qrcode.png")  
+
+print("My QR Code")
